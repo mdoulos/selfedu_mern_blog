cf76d1a72e89c6d27340aece654d737446e9c310
Micah Doulos <micah@studiodoulos.com>
2023-06-17 22:41:45 -0400

    Update

--- api/index.js
+++ api/index.js
@@ -1,14 +1,39 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const User = require('./models/User');
+const bcrypt = require('bcryptjs');
 const app = express();
+
+const salt = bcrypt.genSaltSync(10);
 
 app.use(cors());
 app.use(express.json());
 
-app.post('/register', (req,res) => {
+mongoose.connect('mongodb+srv://blog:6KmGWysOfus0w9O8@cluster0.7pxm9fk.mongodb.net/?retryWrites=true&w=majority');
+
+app.post('/register', async (req,res) => {
     const {username, password} = req.body;
-    res.json({requestData:{username, password}});
+    try {
+        const userDoc = await User.create({
+            username, 
+            password:bcrypt.hashSync(password, salt),
+        });
+        res.json(userDoc);
+    } catch(e) {
+        res.status(400).json(e);
+    }
 });
 
+app.post('/login', async (req,res) => {
+    const {username, password} = req.body;
+    const userDoc = await User.findOne({username});
+    const passOK = bcrypt.compareSync(password, userDoc.password);
+    if (passOK) {
+        // Logged in
+    } else {
+        res.status(400).json("Wrong Credentials");
+    }
+})
+
 app.listen(4000);
-// mongodb+srv://blog:6KmGWysOfus0w9O8@cluster0.7pxm9fk.mongodb.net/?retryWrites=true&w=majority
