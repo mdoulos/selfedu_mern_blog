53050e3a1fd0a452c7cbee4c5a6d198d2077369a
Micah Doulos <micah@studiodoulos.com>
2023-06-17 21:54:49 -0400

    Update

--- api/index.js
+++ api/index.js
@@ -3,11 +3,12 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
-
+app.use(express.json());
 
 app.post('/register', (req,res) => {
-    res.json('test ok');
-    
+    const {username, password} = req.body;
+    res.json({requestData:{username, password}});
 });
 
 app.listen(4000);
+// mongodb+srv://blog:6KmGWysOfus0w9O8@cluster0.7pxm9fk.mongodb.net/?retryWrites=true&w=majority
