53655ff06a80c67386a72224071eb70522306635
Micah Doulos <micah@studiodoulos.com>
2023-06-20 17:02:46 -0400

    Update

--- api/index.js
+++ api/index.js
@@ -4,11 +4,15 @@ const mongoose = require('mongoose');
 const User = require('./models/User');
 const bcrypt = require('bcryptjs');
 const app = express();
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
 const salt = bcrypt.genSaltSync(10);
+const secret = 'kaslnasklnflnl4l3k53lk68389sfjsw0fw0e0';
 
-app.use(cors());
+app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 app.use(express.json());
+app.use(cookieParser());
 
 mongoose.connect('mongodb+srv://blog:6KmGWysOfus0w9O8@cluster0.7pxm9fk.mongodb.net/?retryWrites=true&w=majority');
 
@@ -31,9 +35,29 @@ app.post('/login', async (req,res) => {
     const passOK = bcrypt.compareSync(password, userDoc.password);
     if (passOK) {
         // Logged in
+        jwt.sign({username,id:userDoc._id}, secret, {}, (err,token) => {
+            if (err) throw err;
+            res.cookie('token', token).json({
+                id:userDoc._id,
+                username,
+            });
+        })
+        // res.json();
     } else {
         res.status(400).json("Wrong Credentials");
     }
-})
+});
+
+app.get('/profile', (req,res) => {
+    const {token} = req.cookies;
+    jwt.verify(token, secret, {}, (err,info) => {
+        if (err) throw err;
+        res.json(info);
+    });
+}); 
+
+app.post('/logout', (req,res) => {
+    res.cookie('token', '').json('ok');
+});
 
 app.listen(4000);
